6b036183e96c8a213db90109e4ec0f02b3fca574
cauacouto <cauacouto022@gmail.com>
2026-09-23 15:57:33 -0300

    initialize Codechella application with basic configuration and setup

---
+++ src/test/java/com/couto/dev/codechella/CodechellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couto.dev.codechella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodechellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
