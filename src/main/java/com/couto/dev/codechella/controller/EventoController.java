6f5af227302dd593a263f9e611c09decc7ad8540
cauacouto <cauacouto022@gmail.com>
2026-09-24 15:34:56 -0300

    add CRUD endpoints to EventoController for event management

--- src/main/java/com/couto/dev/codechella/controller/EventoController.java
+++ src/main/java/com/couto/dev/codechella/controller/EventoController.java
@@ -3,11 +3,9 @@ package com.couto.dev.codechella.controller;
 import com.couto.dev.codechella.dto.EventoDto;
 import com.couto.dev.codechella.service.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/eventos")
@@ -17,10 +15,32 @@ public class EventoController {
     private EventoService service;
 
 
-    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping//(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<EventoDto> obterTodos(){
        return service.obeterTodos();
 
     }
 
+    @GetMapping("{id}")
+    public Mono<EventoDto> obterPorId(@PathVariable Long id){
+        return service.obterPorId(id);
+
+    }
+
+    @PostMapping
+    public Mono<EventoDto> cadastrar(@RequestBody EventoDto dto){
+        return service.cadastrar(dto);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deletarPorId(@PathVariable Long id){
+        return service.delete(id);
+    }
+
+
+    @PutMapping("/{id}")
+    public Mono<EventoDto> atualizarPorId(@PathVariable Long id,@RequestBody EventoDto dto){
+        return service.atualizar(id,dto);
+    }
 }
