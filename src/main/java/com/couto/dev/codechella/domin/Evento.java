534d6d1465985efbc7c7f26445bc8f10c5ca44b8
cauacouto <cauacouto022@gmail.com>
2026-09-24 15:34:51 -0300

    refactor Evento class to use Long for id field

--- src/main/java/com/couto/dev/codechella/domin/Evento.java
+++ src/main/java/com/couto/dev/codechella/domin/Evento.java
@@ -8,19 +8,20 @@ import java.time.LocalDate;
 
 @Table(name = "eventos")
 public class Evento {
-// teste
+
     @Id
-    private long id;
+    private Long id;
     private TipoEvento tipo;
     private String nome;
     private LocalDate data;
     private String descricao;
 
-    public long getId() {
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
