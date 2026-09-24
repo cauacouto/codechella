8215b8bd601fc44f03c407739f38a1518bc0b6d4
cauacouto <cauacouto022@gmail.com>
2026-09-24 15:35:03 -0300

    add CRUD endpoints to EventoController for event management

--- src/main/java/com/couto/dev/codechella/dto/EventoDto.java
+++ src/main/java/com/couto/dev/codechella/dto/EventoDto.java
@@ -6,18 +6,28 @@ import com.couto.dev.codechella.domin.Evento;
 import java.time.LocalDate;
 
 public record EventoDto(Long id,
-                        TipoEvento tipo ,
                         String nome,
+                        TipoEvento tipo,
                         LocalDate data,
                         String descricao) {
 
     public static EventoDto toDto(Evento evento){
         return new EventoDto(
                 evento.getId(),
-                evento.getTipo(),
                 evento.getNome(),
+                evento.getTipo(),
                 evento.getData(),
                 evento.getDescricao()
         );
+    }
+
+    public Evento toEntity(){
+        Evento evento = new Evento();
+        evento.setId(this.id);
+        evento.setNome(this.nome);
+        evento.setTipo(this.tipo);
+        evento.setData(this.data);
+        evento.setDescricao(this.descricao);
+        return evento;
     }
 }
