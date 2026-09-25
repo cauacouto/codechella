2bd7d187254a0ad1d1ab15c53eba1ef1b8103f99
cauacouto <cauacouto022@gmail.com>
2026-09-25 15:42:54 -0300

    add event streaming support to EventoController with Sinks for real-time updates

--- src/main/java/com/couto/dev/codechella/controller/EventoController.java
+++ src/main/java/com/couto/dev/codechella/controller/EventoController.java
@@ -3,19 +3,30 @@ package com.couto.dev.codechella.controller;
 import com.couto.dev.codechella.dto.EventoDto;
 import com.couto.dev.codechella.service.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+
+import java.awt.*;
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/eventos")
 public class EventoController {
 
-    @Autowired
-    private EventoService service;
+    private final EventoService service;
+
+    private final Sinks.Many<EventoDto> eventoSink;
+
+    public EventoController(EventoService service) {
+        this.service = service;
+        this.eventoSink = Sinks.many().multicast().onBackpressureBuffer();
+    }
 
 
-    @GetMapping//(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping
     public Flux<EventoDto> obterTodos(){
        return service.obeterTodos();
 
@@ -27,9 +38,17 @@ public class EventoController {
 
     }
 
+
+    @GetMapping(value = "/categoria/{tipo}",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<EventoDto> obterPorTipo(@PathVariable String tipo){
+        return Flux.merge(service.obterPorTipo(tipo),eventoSink.asFlux())
+                .delayElements(Duration.ofSeconds(4));
+    }
+
     @PostMapping
     public Mono<EventoDto> cadastrar(@RequestBody EventoDto dto){
-        return service.cadastrar(dto);
+        return service.cadastrar(dto)
+                .doOnSuccess(e -> eventoSink.tryEmitNext(e));
     }
 
 
