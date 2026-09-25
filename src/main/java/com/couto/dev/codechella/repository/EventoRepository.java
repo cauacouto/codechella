e9d14dc8fbda5b76b987aa39df5456bb42cfcc36
cauacouto <cauacouto022@gmail.com>
2026-09-25 15:43:01 -0300

    add method to EventoRepository for finding events by type

--- src/main/java/com/couto/dev/codechella/repository/EventoRepository.java
+++ src/main/java/com/couto/dev/codechella/repository/EventoRepository.java
@@ -1,7 +1,11 @@
 package com.couto.dev.codechella.repository;
 
+import com.couto.dev.codechella.Enums.TipoEvento;
 import com.couto.dev.codechella.domin.Evento;
+import com.couto.dev.codechella.dto.EventoDto;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface EventoRepository extends ReactiveCrudRepository<Evento,Long> {
+    Flux<Evento> findByTipo(TipoEvento tipoEvento);
 }
